8af50611ca7220c4491c34afff1220bc82e2f14f
SCSFMAC4 <scsfmac4@Admins-MacBook-Air.local>
2017-12-19 16:52:10 -0800

    updated filenames

--- index.js
+++ index.js
@@ -5,22 +5,35 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 // imports the body-parser module
 const bodyParser = require('body-parser')
+// parses url string
 const urlParser = bodyParser.urlencoded({extended: false})
 app.set("view engine", "ejs");
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index');
 });
 
-app.get('/signup', (req, res) => {
-  res.render('signup')
+app.get('/addtodo', (req, res) => {
+  res.render('addtodo');
 });
 
-app.post('/signup', (req, res) => {
+app.post('/addtodo', urlParser, (req, res) => {
+  res.render('addtodo');
+});
 
-})
+app.get('/:todo_id', (req, res) => {
+  res.render('todo');
+});
 
-app.get('/login', (req, res) => {
+app.delete('/:todo_id', (req, res) => {
+  let todoId = req.params.todo_id
+  
+  res.render('todo');
+});
+
+app.post('/login', urlParser, (req, res) => {
+  let { name, email } = req.body
+
   res.render('login')
 });
 
