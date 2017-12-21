d49e073260ffb75838ac9f90330fdafe9e90fe27
DeBray Carpenter <dcarpen2@mail.ccsf.edu>
2017-12-21 10:05:51 -0800

    Update todos.js

--- models/db/todos.js
+++ models/db/todos.js
@@ -1,8 +1,8 @@
 const conn = require('./conn')
 
 const getAllTodos = () => conn.query(`SELECT * FROM todos`)
-const addTodo = (text) => conn.query(`INSERT INTO todos (text) VALUES (text)`, text)
-const getTodoById = (id) => conn.query(`SELECT * FROM todos WHERE id=id`, id)
+const addTodo = (text) => conn.query(`INSERT INTO todos (text) VALUES ($1) RETURNING *`, text)
+const getTodoById = (id) => conn.query(`SELECT * FROM todos WHERE id=$1`, id)
 const getCompletedTodos = () => conn.query(`SELECT * FROM todos WHERE completed=true`)
 const getUncompletedTodos = () => conn.query(`SELECT * FROM todos WHERE completed=false`)
 
